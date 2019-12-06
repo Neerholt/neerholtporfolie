6b023c45e9d5502820445ee400f25e2b1c56c884
Victor Neerholt <victorneerholt@gmail.com>
2019-12-06 14:31:12 +0100

    har lavet en masse om

--- index.php
+++ index.php
@@ -12,100 +12,44 @@
                <li><a href="index.php">Home</a></li>
                <li><a href="#">Contact</a></li>
                <li><a href="#">Projects</a></li>
+               <li><a href="#">About</a></li>
              </ul> 
            </nav>
        </header>
        
        <main>
-           <div id="container">
+           <div id="top-container">
                  <p>Some text some text some text some text..</p>
   <p>Some text some text some text some text..</p>
   <p>Some text some text some text some text..</p>
-  <p>Some text some text some text some text..</p>
-  <p>Some text some text some text some text..</p>
-  <p>Some text some text some text some text..</p>
-  <p>Some text some text some text some text..</p>
-  <p>Some text some text some text some text..</p>
-  <p>Some text some text some text some text..</p>
-  <p>Some text some text some text some text..</p>
-  <p>Some text some text some text some text..</p>
-  <p>Some text some text some text some text..</p>
-  <p>Some text some text some text some text..</p>
-  <p>Some text some text some text some text..</p>
-  <p>Some text some text some text some text..</p>
-  <p>Some text some text some text some text..</p>
-  <p>Some text some text some text some text..</p>
-  <p>Some text some text some text some text..</p>
-  <p>Some text some text some text some text..</p>
-  <p>Some text some text some text some text..</p> 
-  <p>Some text some text some text some text..</p>
-  <p>Some text some text some text some text..</p>
-  <p>Some text some text some text some text..</p>
-  <p>Some text some text some text some text..</p>
-  <p>Some text some text some text some text..</p>
-  <p>Some text some text some text some text..</p>
-  <p>Some text some text some text some text..</p>
-  <p>Some text some text some text some text..</p>
-  <p>Some text some text some text some text..</p>
-  <p>Some text some text some text some text..</p>
-  <p>Some text some text some text some text..</p>
-  <p>Some text some text some text some text..</p>
-  <p>Some text some text some text some text..</p>
-  <p>Some text some text some text some text..</p>
-  <p>Some text some text some text some text..</p>
-  <p>Some text some text some text some text..</p>
-  <p>Some text some text some text some text..</p>
-  <p>Some text some text some text some text..</p>
-  <p>Some text some text some text some text..</p>
-  <p>Some text some text some text some text..</p>
-  <p>Some text some text some text some text..</p>
-  <p>Some text some text some text some text..</p>
-  <p>Some text some text some text some text..</p>
-  <p>Some text some text some text some text..</p>
-  <p>Some text some text some text some text..</p>
-  <p>Some text some text some text some text..</p>
-  <p>Some text some text some text some text..</p>
-  <p>Some text some text some text some text..</p>
-  <p>Some text some text some text some text..</p>
-  <p>Some text some text some text some text..</p>
-  <p>Some text some text some text some text..</p>
-  <p>Some text some text some text some text..</p>
-  <p>Some text some text some text some text..</p>
-  <p>Some text some text some text some text..</p>
-  <p>Some text some text some text some text..</p>
-  <p>Some text some text some text some text..</p>
-   <p>Some text some text some text some text..</p>
-  <p>Some text some text some text some text..</p>
-  <p>Some text some text some text some text..</p>
-  <p>Some text some text some text some text..</p>
-  <p>Some text some text some text some text..</p>
-  <p>Some text some text some text some text..</p>
-  <p>Some text some text some text some text..</p>
-   <p>Some text some text some text some text..</p>
-  <p>Some text some text some text some text..</p>
-  <p>Some text some text some text some text..</p>
-  <p>Some text some text some text some text..</p>
-  <p>Some text some text some text some text..</p>
-  <p>Some text some text some text some text..</p>
-  <p>Some text some text some text some text..</p>
-  <p>Some text some text some text some text..</p>
-  <p>Some text some text some text some text..</p>
-  <p>Some text some text some text some text..</p>
-  <p>Some text some text some text some text..</p>
-  <p>Some text some text some text some text..</p>
-  <p>Some text some text some text some text..</p>
-  <p>Some text some text some text some text..</p>
            </div>
+           
+           <a href="#"><div id="trippel-container">
+               <p>Left</p>
+           </div></a>
+           
+           
+           <a href="#"><div id="trippel-container">
+               <p>center</p>
+           </div></a>
+           
+           <a href="#"><div id="trippel-container">
+               <p>right</p>
+           </div></a>
+           
        </main>
        
+
        <footer>
            <div id="footer">
-               <table>
-                   <tr><td><a href="url">Twitter</a></td></tr>
-                    <tr><td><a href="url">Steam</a></td></tr>
-                     <tr><td><a href="url">Facebook</a></td></tr>
-                      <tr><td><a href="url">LinkedIn</a></td></tr>
-               </table>
+               <nav>
+                 <ul>
+                   <li><a href="index.php">Twitter</a></li>
+                   <li><a href="#">Steam</a></li>
+                   <li><a href="#">Facebook</a></li>
+                   <li><a href="#">LinkedIn</a></li>
+                </ul> 
+               </nav>
            </div>
        </footer>
        
