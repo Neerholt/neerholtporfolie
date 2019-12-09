e25f411584ff3bbc2b486e97389249306703fac1
Victor Neerholt <victorneerholt@gmail.com>
2019-12-09 13:54:30 +0100

    lavet noget idk kev

--- makeartikel.php
+++ makeartikel.php
@@ -36,7 +36,7 @@ mysqli_close($connect);
 ?>    
 <html>
     <head>
-        <title>Portfolie</title>
+        <title>Opret Artikel</title>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link rel="stylesheet" type="text/css" href="stylecss.css">
