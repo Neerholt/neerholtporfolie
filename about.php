d23b2cc3b9997deb866eff97e26c9a98cd56738a
Victor Neerholt <victorneerholt@gmail.com>
2019-12-10 15:12:48 +0100

    l

--- about.php
+++ about.php
@@ -3,7 +3,7 @@
       <link rel="stylesheet" type="text/css" href="style.css">
       <meta name="viewport" content="width=device-width, initial-scale=1.0">
       <link href="https://fonts.googleapis.com/css?family=Roboto&display=swap" rel="stylesheet"><!--Google stylesheet-->
-      <title>Neerholt.it</title>
+      <title>About</title>
    </head>
    <body>
        <header>
