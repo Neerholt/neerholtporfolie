5642d1e717c181c71ec184a4994864de1775c601
Victor Neerholt <victorneerholt@gmail.com>
2019-12-10 13:28:08 +0100

    kage er godt

--- projects.php
+++ projects.php
@@ -1,7 +1,7 @@
 <?php include 'databaseconn.php';?>
 <html>
    <head>
-      <link rel="stylesheet" type="text/css" href="style.css">
+       <link rel="stylesheet" type="text/css" href="stylepro.css">
       <meta name="viewport" content="width=device-width, initial-scale=1.0">
       <link href="https://fonts.googleapis.com/css?family=Roboto&display=swap" rel="stylesheet"><!--Google stylesheet-->
       <title>Projects</title>
